2a564e7196c71359f3d04233fe22c32fa44b4eee
AbaanAnsari <abaanansari1427@gmail.com>
2026-08-16 13:50:34 +0530

    updated ticket-controller and added getticketbyid in ticket-service

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -1,15 +1,17 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { getTicketById } from '../services/ticket.service.js';
 import { StatusCodes } from 'http-status-codes';
+import ApiResponse from '../utils/ApiResponse.js';
 
 export const getTicket = asyncHandler(async (req, res) => {
     const { ticketId } = req.params;
 
     const ticket = await getTicketById(ticketId);
 
-    return res.status(StatusCodes.OK).json({
-        success: true,
-        message: 'Ticket retrieved successfully.',
-        data: ticket,
-    });
+    return res.status(StatusCodes.CREATED).json(
+        new ApiResponse(StatusCodes.CREATED, 'Ticket retrived successfully', {
+            data:ticket
+        })
+    );
+    
 });
